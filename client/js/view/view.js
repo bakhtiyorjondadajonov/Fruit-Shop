9dbdf2cf122e49b486b947fb1bbaed57bb07bb6f
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2022-08-11 15:20:13 +0900

    section about is done

--- client/js/view/view.js
+++ client/js/view/view.js
@@ -1,7 +1,8 @@
 const navBarCarousel = document.querySelector(".navigation__bar__carousel");
+const bestSellerCarousel = document.querySelector(".carousel-bestseller");
 var previousButton = document.querySelector(".btn--nav-left");
 var nextButton = document.querySelector(".btn--nav-right");
-console.log("nextButton: ", nextButton);
+
 export const carouselFn = (elem, btnLeft, btnRight) => {
   if (elem) {
     const flkty = new Flickity(elem, {
@@ -13,17 +14,18 @@ export const carouselFn = (elem, btnLeft, btnRight) => {
     });
 
     // previous
-    btnLeft.addEventListener("click", function () {
-      flkty.previous(true);
-      console.log("btnLeft: ", btnLeft);
-    });
-
-    // next
-
-    btnRight.addEventListener("click", function () {
-      flkty.next(true);
-      console.log("btnRight: ", btnRight);
-    });
+    if (btnLeft && btnRight) {
+      btnLeft.addEventListener("click", function () {
+        flkty.previous(true);
+        console.log("btnLeft: ", btnLeft);
+      });
+      // next
+      btnRight.addEventListener("click", function () {
+        flkty.next(true);
+        console.log("btnRight: ", btnRight);
+      });
+    }
   }
 };
 carouselFn(navBarCarousel, previousButton, nextButton);
+carouselFn(bestSellerCarousel);
